8c13a85b9602584b2f6b257fc8c850d79cd93001
Jose B. Gomes <jose.gomes@cybertill.co.uk>
2018-08-31 16:10:33 +0100

    Fixing bug with write-good

--- write-good.js
+++ write-good.js
@@ -5,15 +5,16 @@ const writeGood = require('write-good');
 
 const data = yml.safeLoad(fs.readFileSync('./index.yml', 'utf8'));
 
-const loopArray = data => data.map(_ => suggestions(_));
-const loopObject = data => Object.keys(data).map(_ => suggestions(data[_]));
+const loopArray = arr => arr.map(_ => suggestions(_));
+const loopObject = obj => Object.keys(obj).map(_ => suggestions(obj[_]));
 
-const suggestions = (data) => {
-  if (typeof data === 'object') {
-    return Array.isArray(data) ? loopArray(data) : loopObject(data);
+const suggestions = (segment) => {
+  if (typeof segment === 'object') {
+    return Array.isArray(segment) ? loopArray(segment) : loopObject(segment);
   }
 
-  return writeGood(data, { eprime: true });
+  return writeGood(`${segment}`, { eprime: true });
 };
 
 console.log(util.inspect(suggestions(data), false, null));
+
